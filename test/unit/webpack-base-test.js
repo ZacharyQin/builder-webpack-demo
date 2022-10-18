666d5c84b3a7a9bdfd6da3d348c14d76f933c7cc
ZacharyQin <yuihjk015328@gmail.com>
2022-10-18 15:24:12 +0800

    fix:  clear local path, replaceAll -> replace

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
-
+const path = require('path')
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js');
-    console.log(baseConfig);
+    const basePath = path.join(__dirname, '../smoke/template/src/')
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, `H:/Learn/GeekTime/54-玩转webpack/my-project/builder-webpack/test/smoke/template/src/index/index.js`)
-        assert.equal(baseConfig.entry.search, `H:/Learn/GeekTime/54-玩转webpack/my-project/builder-webpack/test/smoke/template/src/search/index.js`)
+        assert.equal(baseConfig.entry.index, path.join(basePath, 'index/index.js').replace(/\\/g, '/'))
+        assert.equal(baseConfig.entry.search, path.join(basePath, 'search/index.js').replace(/\\/g, '/'))
     })
 })
